3f2caf213efa1f5d66d508578bb618ff95094956
pawellski <skibaapawell@gmail.com>
2021-06-12 20:59:53 +0200

    Limited WriteFile to one option

--- WriteFile.c
+++ WriteFile.c
@@ -5,50 +5,19 @@
 
 #define MAX_STR_SIZE 200
 
-void print_to_screen(int n, grid_t *to_write, dimension_t *dim)
-{
-    printf("PLANSZA PO %d ITERACJI\n", n);
-    for(int i=1; i<(dim->rows)-1; ++i)
-    {
-        for(int j=1; j<(dim->columns)-1; ++j)
-        {
-            printf("%c ", to_write->cells[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void fill_in_file(int n, char *filename, grid_t *to_write, dimension_t *dim)
+void fill_in_file(char *filename, grid_t *to_write, dimension_t *dim)
 {
     FILE *fp = fopen(filename, "a");
 
-    fprintf (fp, "PLANSZA PO %d ITERACJI\n", n);
+    fprintf (fp, "WYNIK\n");
     for(int i=1; i<(dim->rows)-1; ++i)
     {
         for(int j=1; j<(dim->columns)-1; ++j)
         {
-            fprintf (fp, "%c ", to_write->cells[i][j]);
+            fprintf (fp, "%c", to_write->cells[i][j]);
         }
         fprintf(fp, "\n");
     }
     fclose( fp );
 
-}
-
-void
-make_gif_command( double delay, int gen_num, char *gifFilename)
-{
-	char str[MAX_STR_SIZE];
-	sprintf(str, "convert -delay %g -loop 0 $(for i in $(seq 1 1 %d); do echo ./png_images/image_${i}.png; done) %s", delay, gen_num, gifFilename);
-	const char * command = str;
-	system(command);
-}
-
-void
-delete_png_from_dir( void )
-{
-	system("mkdir png_images");
-	system("cd png_images");
-   	system("rm -rf /png_images/*");
-    system("cd ..");
 }
--- WriteFile.h
+++ WriteFile.h
@@ -3,10 +3,6 @@
 
 #include "Game.h"
 
-void fill_in_file(int n, char *filename, grid_t *to_write, dimension_t *dim);
-void print_to_screen(int n, grid_t *to_write, dimension_t *dim);
-void make_gif_command( double delay, int gen_num, char *gifFilename);
-void delete_png_from_dir( void );
-
+void fill_in_file(char *filename, grid_t *to_write, dimension_t *dim);
 
 #endif // WRITEFILE_H_INCLUDED
